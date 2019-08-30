45ec4556216c6caab1c9de743d03bf7b7f61b029
h4rdl1nk <imthedanger@protonmail.com>
2019-08-30 08:32:57 +0200

    Added error control

--- code/main.py
+++ code/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import requests
+import csv
 import xml.etree.ElementTree as ET
 
 
@@ -23,12 +24,19 @@ except Exception:
 
 # Get IB API info
 
+print("Obtaining report reference code with token %s and query ID %s" % (ibToken,ibFlexId))
+
 referenceParams = {'t': ibToken, 'q': ibFlexId, 'v': '3'}
 referenceReq = requests.get('https://gdcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.SendRequest', params=referenceParams)
 
 if referenceReq.status_code == 200:
     referenceTree = ET.fromstring(referenceReq.text)
     referenceCode = referenceTree.findtext('ReferenceCode')
+    if referenceTree.findtext('Status') != "Success":
+      print("Got [%s] result from request [%s]: %s" % (referenceTree.findtext('Status'),referenceTree.findtext('ErrorCode'),referenceTree.findtext('ErrorMessage')))
+      sys.exit()
+    else:
+      print("Got reference code %s" % (referenceCode))
 else:
     print("Error getting reference code %d" % (referenceReq.status_code))
     sys.exit()
@@ -40,7 +48,10 @@ flexParams = {'t': ibToken, 'q': referenceCode, 'v': '3'}
 flexReq = requests.get('https://gdcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.GetStatement', params=flexParams)
 
 if flexReq.status_code == 200:
-    print(flexReq.text)
+    flexCsv = csv.reader(flexReq.text.splitlines()) 
+    for line in flexCsv:
+      print(line)
 else:
     print("Error getting flex query result %d" % (flexReq.status_code))
     sys.exit()
+
