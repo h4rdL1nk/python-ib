49fae1dea139600ba827900cda9d196b24a48448
luismiguelsaez <luismiguelsaez@protonmail.com>
2019-09-30 13:33:16 +0200

    Added function

--- code/utils.py
+++ code/utils.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import datetime
 import csv
+import re
 from prettytable import PrettyTable
 
 def csvToTable( csvString ):
@@ -38,3 +39,38 @@ def htmlToImage( htmlString ):
 
     return convertImgDest
 
+
+def csvDividendsToObj( csvString ):
+
+    dividendsDict = []
+
+    flexCsv = csv.reader(csvString.splitlines()) 
+
+    row_count = 0
+    for row in flexCsv:
+        if len(row) > 0:
+            if row_count == 0:
+                headers = []
+                for header in row:
+                    headers.append(header)
+
+            if row[headers.index('ActivityCode')] == 'DIV' and row[headers.index('LevelOfDetail')] == 'Currency':
+                desc = row[headers.index('ActivityDescription')]
+                symbol = row[headers.index('Symbol')]
+                amount = row[headers.index('Amount')]
+
+                matches = re.match("(.*)\(([A-Z0-9]+)\).*([0-9]+.[0-9]+)",desc)
+
+                if matches:
+                    isin = matches.group(2)
+                    amount_per_share = matches.group(3)
+                else:
+                    isin = "n/a"
+                    amount_per_share = "n/a"
+
+                dividendObj = { "symbol": symbol, "shares": str((float(amount) / float(amount_per_share))), "amountSh": str(amount_per_share), "amountTotal": str(amount)}
+                dividendsDict.append(dividendObj)
+
+        row_count = row_count + 1
+
+    return dividendsDict
