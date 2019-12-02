80e4a4963f5860be27cc40b80729ce2c95d69a08
luismiguelsaez <luismiguelsaez@protonmail.com>
2019-12-02 10:22:02 +0100

    Fixed if statement syntax

--- code/main.py
+++ code/main.py
@@ -126,7 +126,7 @@ def _sendUpdatesDaily( bot, userId, reports, token="dummy" ):
             open('/tmp/alert-bot.stat','w').write(todayHourStr)
 
 
-        if launchHourly and datetime.datetime.now().hour == 7
+        if launchHourly and datetime.datetime.now().hour == 7:
             logging.info( "Executing daily launch for matched hour [7]" )
             _sendChargedDividends( bot, userId, 'DIVS_RETR_DAILY', token )
 
